ba5d305dc885241945a9ece29bde29c2dbfcaa8e
kywch <choe.kyoung@gmail.com>
2024-11-15 09:58:25 -0800

    fix setup

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
+    packages=["brax_trainer"],
     ext_modules=cythonize("brax_trainer/c_gae.pyx"),
     include_dirs=[numpy.get_include()],
 )
